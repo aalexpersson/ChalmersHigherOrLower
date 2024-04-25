5b819e2580862cac5cc0beb5f7cdfd034d9a7f2e
aalexpersson <alex.persson0408@gmail.com>
2024-04-25 20:24:00 +0200

    efefefe

--- server/src/db/conn.ts
+++ server/src/db/conn.ts
@@ -3,10 +3,10 @@ import { readFileSync } from 'fs';
 
 
 
-const password = readFileSync('./src/db/dbPassword.txt', 'utf-8')
+const db = readFileSync('./src/db/util.txt', 'utf-8')
 
 async function makeConnection() : Promise<Connection>{
-    return createConnection(`mongodb+srv://pontusen:${password}@chalmershigherlower.akkne3j.mongodb.net/?retryWrites=true&w=majority`)
+    return createConnection(`mongodb+srv://pontusen:${db}@chalmershigherlower.akkne3j.mongodb.net/?retryWrites=true&w=majority`)
 }
 
 export const conn : Promise<Connection> = makeConnection()
